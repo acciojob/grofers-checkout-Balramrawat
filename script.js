3c6861bb791e55b0148ae1ccbf7b20569891d4a0
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-02-17 22:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+
+	let prices = document.querySelectorAll(".price");
+                let total = 0;
+                
+                prices.forEach(price => {
+                    total += parseFloat(price.textContent) || 0;
+                });
+                
+                let table = document.querySelector("table");
+                let totalRow = document.createElement("tr");
+                let totalCell = document.createElement("td");
+                // totalCell.colspan = "1";
+                totalCell.textContent = "Total Price: Rs " + total;
+                
+                totalRow.appendChild(totalCell);
+                table.appendChild(totalRow);
   
 };
 
